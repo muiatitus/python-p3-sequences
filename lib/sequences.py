e8b0f3f3536ca270faecafb4b214f07afe817d4c
Titus <titus@ubuntu23.04>
2023-08-25 13:57:44 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+    
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        while len(fibonacci_sequence) < length:
+            next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_value)
+        print(fibonacci_sequence)
